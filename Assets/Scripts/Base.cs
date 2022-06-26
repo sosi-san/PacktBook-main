b5e48be83795c2fc4c1d4a7503e44de4c1af2159
Adam Woska <ademwoska@gmail.com>
2022-06-26 13:28:43 +0200

    Initial Commit

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace Woska
+{
+    public class Base : MonoBehaviour
+    {
+        #region Public Fields
+        #endregion
+
+        #region Unity Method
+
+        private void Awake()
+        {
+            
+        }
+        private void Start()
+        {
+            
+        }
+        private void Update()
+        {
+            
+        }
+
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+        #endregion
+    }
+}
